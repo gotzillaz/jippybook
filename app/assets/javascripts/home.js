eb5c7bd7addeb1bea8e4440f1cc4a6bc28327fa4
Gotzillaz Chinthanet <got.bodin@gmail.com>
2015-05-03 20:46:21 +0700

    add home.js in assets/javascripts

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,8 @@
+    document.body.style.backgroundColor = "red";
+alert("xx");
+    console.log("asdf")
+$(document).on('page:load', function() {
+    document.body.style.backgroundColor = "red";
+    console.log("asdf")
+    // your stuff here
+});
