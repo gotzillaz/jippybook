9b2df1d259c15bca70baa2e384615c51a2b4a0fd
Gotzillaz Chinthanet <got.bodin@gmail.com>
2015-05-07 18:12:32 +0700

    can register new user from /registration

--- app/controllers/registration/registration_controller.rb
+++ app/controllers/registration/registration_controller.rb
@@ -1,21 +1,31 @@
 class Registration::RegistrationController < ApplicationController
   def index
+    @user = User.new
   end
 
   def create
-    tmp = params[:user]
+    reg_user = params[:user]
     #p tmp
-    p tmp['username']
+    p reg_user['username']
     @user = User.new do |u|
-      u.username = tmp['username']
-      u.password = tmp['password']
-      u.password_confirmation = tmp['password_confirmation']
-      u.email = tmp['email']
+      u.username = reg_user['username']
+      u.password = reg_user['password']
+      u.password_confirmation = reg_user['password_confirmation']
+      u.email = reg_user['email']
     end
-    p user.save
-    
+    if @user.save
+      redirect_to '/'
+    else
+      p "Failed"
+      p @user.errors
+      render 'index'
+    end
     #user = User.create(params[:user])
-    
-    redirect_to '/registration'
   end
+  
+  private 
+    def user_params
+      params.require(:user).permit(:username, :email, :password,
+                                   :password_confirmation)
+    end
 end
