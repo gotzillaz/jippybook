03b6bc2ea13afcdffe31cc73eebfaba415b4fced
Gotzillaz Chinthanet <got.bodin@gmail.com>
2015-05-08 22:42:35 +0700

    prepare inventory controller for add data

--- app/controllers/admin/inventory_controller.rb
+++ app/controllers/admin/inventory_controller.rb
@@ -1,8 +1,22 @@
 class Admin::InventoryController < ApplicationController
   def index
+    @book = Book.all()
+  end
+
+  def create
+    @book = Book.new do |b|
+      b.name = params[:book][:name]
+      b.description = params[:book][:description]
+      b.img = params[:book][:img]
+      b.img = params[:book][:price]
+    end
+    @book.save
+    render 'new' 
   end
 
   def new
+    @book = Book.new
+    @category_list = Category.all()
   end
  
 end
